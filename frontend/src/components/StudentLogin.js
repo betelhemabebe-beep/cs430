de73e15a62b15c1039627b848129c048fe26bc86
Kamsi Amaeshi <146353641+Kamiams@users.noreply.github.com>
2024-12-01 23:11:34 -0600

    Update StudentLogin.js
    changed font, buttons and colours

--- frontend/src/components/StudentLogin.js
+++ frontend/src/components/StudentLogin.js
@@ -34,6 +34,7 @@ function StudentLogin() {
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
         </svg>
       </div>
+      <div style={styles.cardCentered}>
       <h2 style={styles.title}>Student Login</h2>
       <form onSubmit={handleSubmit} style={styles.form}>
         <input
@@ -53,6 +54,7 @@ function StudentLogin() {
         />
         <button type="submit" style={styles.button}>Login</button>
       </form>
+      </div>
     </div>
   );
 }
@@ -65,10 +67,10 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
-    background: 'linear-gradient(135deg,#510C76, #ffffff)',
-    fontFamily: 'Arial, sans-serif',
+    background: 'linear-gradient(to top,#510C76, #ffffff)',
+    fontFamily: 'Quincy CF, sans-serif',
     padding: '20px',
-    color: '#fff',
+    color: '#510C76',
   },
   arrowContainer: {
     position: 'absolute',
@@ -79,9 +81,21 @@ const styles = {
   arrowIcon: {
     width: '24px',
     height: '24px',
-    color: '#fff',
+    color: '#000',
   },
-  title: {
+  cardCentered: {
+    border: '1px solid rgba(255, 255, 255, 0.2)',
+    padding: '40px',
+    width: '100%',
+    maxWidth: '400px',
+    borderRadius: '12px',
+    textAlign: 'center',
+    backgroundColor: '#000000',
+    boxShadow: '0 6px 30px rgba(0, 0, 0, 0.4)',
+    transition: 'transform 0.3s ease',
+    cursor: 'pointer',
+  },
+ title: {
     fontSize: '2.5em',
     marginBottom: '20px',
     color: '#800080',
@@ -90,28 +104,20 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     gap: '15px',
-    alignItems: 'center',
-    width: '100%',
-    maxWidth: '400px',
-    padding: '20px',
-    backgroundColor: '#cbaacb',
-    borderRadius: '12px',
-    boxShadow: '0 6px 16px rgba(0, 0, 0, 0.2)',
   },
   input: {
     padding: '10px',
-    width: '100%',
-    borderRadius: '8px',
-    border: '1px solid rgba(255, 255, 255, 0.5)',
+    borderRadius: '12px',
+    border: 'none',
     fontSize: '16px',
     outline: 'none',
   },
   button: {
-    padding: '10px 20px',
+    padding: '10px',
     backgroundColor: '#800080',
     color: '#fff',
     border: 'none',
-    borderRadius: '8px',
+    borderRadius: '12px',
     cursor: 'pointer',
     fontSize: '1em',
     transition: 'background-color 0.3s ease',
